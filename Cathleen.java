9b607966d1aa641586b74d5039b4e98aaba3a33a
root <kongc@chelmsford.k12.ma.us>
2025-06-09 10:07:49 -0400

    Introducing 2nd bug

--- Cathleen.java
+++ Cathleen.java
@@ -2,6 +2,7 @@ package github_demo;
 
 public class Cathleen {
     public static void main(String[] args) {
-        System.out.println("This line has an error");
+        System.out.println("This line no longer has an error");
+        System.out.printl("This line has an error now!");
     }
 }
