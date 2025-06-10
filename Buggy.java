7bf6b33e973c26febde1a2622858fd4de384e1f5
root <kongc@chelmsford.k12.ma.us>
2025-06-10 12:23:00 -0400

    6'7 command

--- Buggy.java
+++ Buggy.java
@@ -1,5 +1,10 @@
 public class Buggy {
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(add(6, 7));
     }
 }
